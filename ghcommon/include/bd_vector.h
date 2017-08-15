ba2c35204aaa3a8cca0c1141c154daa5514c99bb
Le Minh Tan (RBVH/ENG25) <Tan.LeMinh@vn.bosch.com>
2017-08-15 11:37:05 +0700

    vector

--- ghcommon/include/bd_vector.h
+++ ghcommon/include/bd_vector.h
@@ -8,6 +8,8 @@
 #ifndef INCLUDE_BD_VECTOR_H_
 #define INCLUDE_BD_VECTOR_H_
 
+#include <stdio.h>
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <memory>
@@ -25,9 +27,166 @@ namespace Bodhi
         mData((T*)(new uint8_t[sizeof(T) * mAllocated])),
         mSize(0)
       {
+         printf("vector 0\n");
+      }
 
+      explicit vector(unsigned allocated)
+      : mAllocated(allocated),
+        mData((T*)(new uint8_t[sizeof(T) * mAllocated])),
+        mSize(0)
+      {
+         printf("vector 1\n");
+      }
+
+      vector(const vector<T>& rhs)
+      : mAllocated(0),
+        mData(NULL),
+        mSize(0)
+      {
+         *this = rhs;
+      }
+
+      ~vector()
+      {
+         if(mData)
+         {
+            clear();
+
+            delete[] (uint8_t*)mData;
+            mData = NULL;
+         }
+      }
+
+      T& operator [] (unsigned int ind)
+      {
+         if (ind >= mSize)
+            return *(T*)NULL;
+
+         return mData[ind];
+      }
+
+      const T& operator [] (unsigned int ind) const
+      {
+         if (ind >= mSize)
+            return *(T*)NULL;
+
+         return mData[ind];
+      }
+
+      vector<T>& operator = (const vector<T>& rhs)
+      {
+         clear();
+
+         if (mAllocated < rhs.mSize)
+         {
+            sizeup(rhs.mSize);
+         }
+         for (unsigned int i = 0; i < rhs.mSize; i++)
+         {
+            new (mData + i) T(rhs.mData[i]);
+         }
+
+         mSize = rhs.mSize;
+         return *this;
+      }
+
+      void push_back(const T& val)
+      {
+         printf("push_back\n");
+         if (mSize == mAllocated)
+         {
+            sizeup();
+         }
+         new(mData + mSize) T(val);
+
+         mSize++;
+      }
+
+      void erase(unsigned int ind)
+      {
+         if (ind >= mSize)
+            abort();
+
+         for (unsigned int i = ind; i < mSize - 1; i++)
+         {
+            mData[mSize - 1].~T();
+            mSize--;
+         }
+      }
+
+      void reserve(unsigned int size)
+      {
+         if (mAllocated < size)
+         {
+            sizeup(size);
+         }
+      }
+
+      void resize(unsigned int newSize)
+      {
+         if (mAllocated < newSize)
+         {
+            if (newSize < mAllocated * 2)
+            {
+               sizeup();
+            } else {
+               sizeup(newSize);
+            }
+         }
+
+         for (unsigned int i = newSize; i < mSize; i++)
+         {
+            mData[i].~T();
+         }
+
+         for (unsigned int i = mSize; i < newSize; i++)
+         {
+            new(mData + i) T();
+         }
+         mSize = newSize;
+      }
+
+      unsigned int size() const { return mSize; }
+
+      bool empty() const { return mSize == 0; }
+
+      void clear()
+      {
+         for (unsigned int i = 0; i < mSize; i++)
+         {
+            mData[i].~T();
+         }
+         mSize = 0;
       }
    private:
+      void sizeup(unsigned int newSize = 0)
+      {
+         printf("sizeup\n");
+         if (newSize == 0)
+         {
+            if (mAllocated)
+            {
+               newSize = mAllocated * 2;
+            } else {
+               newSize = DEFAULT_ALLOCATION;
+            }
+         }
+
+         T* temp = (T*)(new uint8_t[sizeof(T) * newSize]);
+
+         if (mData)
+         {
+            for (unsigned int i = 0; i < mSize; i++)
+            {
+               new(temp + i) T(mData[i]);
+               mData[i].~T();
+            }
+            delete[] (uint8_t*)mData;
+         }
+         mData = temp;
+         mAllocated = newSize;
+      }
+
       unsigned int mAllocated;
       T* mData;
       unsigned int mSize;
---
+++ ghcommon/test/vectorTest.cpp
@@ -0,0 +1,50 @@
+/*
+ * vectorTest.cpp
+ *
+ *  Created on: Aug 15, 2017
+ *      Author: lia1hc
+ */
+
+#include "../include/bd_vector.h"
+
+int main(void)
+{
+   Bodhi::vector<int> test;
+   test.push_back(17);
+   test.push_back(12);
+   test.push_back(1991);
+   test.push_back(13);
+   test.push_back(11);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+   test.push_back(1991);
+
+   return 0;
+}
